94a58bdd3aa9b03f51bce5419b267128b51c2355
manisha poddar <manisha.pdr@gmail.com>
2016-09-08 20:05:41 -0500

    YouTubeIntegration

---
+++ YouTubeIntegration/app/src/main/java/com/codingblocks/youtubeintegration2/Config.java
@@ -0,0 +1,10 @@
+package com.codingblocks.youtubeintegration2;
+
+/**
+ * Created by Manisha Poddar on 7/28/2016.
+ */
+public class Config {
+
+    public static final String DEVELOPER_KEY  = "AIzaSyB6zAvjriU4FgXa3mFHj5f6NS3edvO51ow";
+    public static final String YOUTUBE_VIDEO_CODE = "a4NT5iBFuZs";
+}
